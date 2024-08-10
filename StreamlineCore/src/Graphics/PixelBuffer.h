aa6fde73060b1fe2dfbdade661f624f55dbd56ed
amayesingnathan <amayesingnathan123@gmail.com>
2024-08-10 15:48:46 +0100

    Add pixel buffer object

---
+++ StreamlineCore/src/Graphics/PixelBuffer.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Texture.h"
+
+namespace slc {
+
+	struct Pixel
+	{
+		int8_t r, g, b, a;
+	};
+
+	class PixelBuffer : public IRenderable
+	{
+	public:
+		PixelBuffer(int width, int height);
+		~PixelBuffer();
+
+		void Lock();
+		void Unlock();
+
+		uint32_t GetTextureID() const override { return mTexture->GetTextureID(); }
+
+		Pixel& At(size_t x, size_t y);
+		const Pixel& At(size_t x, size_t y) const;
+
+		Pixel* Get(size_t offset = 0) { return mPixels + offset; }
+		const Pixel* Get(size_t offset = 0) const { return mPixels + offset; }
+		void Set(const Pixel& colour);
+		void Set(const Pixel& colour, size_t size, size_t offset);
+
+	private:
+		uint32_t mRendererID = 0;
+		int mSize = 0;
+		int mWidth = 0, mHeight = 0;
+
+		bool mLocked = false;
+
+		Ref<Texture2D> mTexture = nullptr;
+		Pixel* mPixels = nullptr;
+	};
+}
